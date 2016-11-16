42c598c3ad5ac607137dd8536646fdca6be42fd9
Jeena <kjeenji@gmail.com>
2016-11-16 22:22:28 +0530

    Seperating twitter credentials

--- config/initializers/twitter_config.rb
+++ config/initializers/twitter_config.rb
@@ -1,8 +1,8 @@
 require 'twitter'
 
 CLIENT = Twitter::REST::Client.new do |config|
-  config.consumer_key        = "V7OXBIcvGJvHPm9EzdArKyK8Q"
-  config.consumer_secret     = "PLBpixsGF1XOdL3f8EZbJnnSXJuV1IT1oV611gVqWahV4i87bc"
-  config.access_token        = "798463435474206720-rEdgJ4amOieFxea9KvfjnhfgxtMlgis"
-  config.access_token_secret = "rRkrjnBFMC0a7zJeaZpJx7AdgTmN284lFnMW8xo6wjnFW"
+  config.consumer_key        = Twitter::CONSUMERKEY
+  config.consumer_secret     = Twitter::CONSUMERSECRET
+  config.access_token        = Twitter::ACCESSTOKEN
+  config.access_token_secret = Twitter::ACCESSTOKENSECRET
 end
